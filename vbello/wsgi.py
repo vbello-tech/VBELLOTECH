924c6f091d1a4d99c8564ccffc855870e2133b28
Vbello-Tech <69384115+vbello-tech@users.noreply.github.com>
2024-01-26 05:52:22 +0100

    allowed host

--- vbello/wsgi.py
+++ vbello/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vbello.settingsproduction')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vbello.settings')
 
 application = get_wsgi_application()
 
