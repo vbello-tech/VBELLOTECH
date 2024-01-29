3245448f89d5b1956cfdffa17d28de273f552b04
Vbello-Tech <69384115+vbello-tech@users.noreply.github.com>
2024-01-29 05:27:19 +0100

    fixing templates issues

--- vbello/settings.py
+++ vbello/settings.py
@@ -79,7 +79,7 @@ ROOT_URLCONF = 'vbello.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
