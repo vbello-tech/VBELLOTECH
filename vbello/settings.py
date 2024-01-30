cf8713c09887e565b4d8db50d85bc02353541162
Vbello-Tech <69384115+vbello-tech@users.noreply.github.com>
2024-01-30 02:45:36 +0100

    TITLE

--- vbello/settings.py
+++ vbello/settings.py
@@ -6,6 +6,9 @@ import os
 
 from pathlib import Path
 from decouple import config
+import dj_database_url
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,6 +29,11 @@ ALLOWED_HOSTS = [
     'vbellotech.pythonanywhere.com',
 ]
 
+DATABASES = {
+    'default':  dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 INSTALLED_APPS = [
     'myapp',
